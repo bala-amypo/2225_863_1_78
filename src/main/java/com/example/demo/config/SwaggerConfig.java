e0405a8107678df801d3fa21cc67875c37dbfe41
bala-amypo <bala@amypo.in>
2025-12-26 09:49:00 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-/*package com.example.demo.config;
+package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -28,44 +28,4 @@ public class SwaggerConfig {
     }
   
 }
-*/
-package com.example.OneToMany.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-
-        SecurityScheme bearerAuth = new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        return new OpenAPI()
-                .info(new Info()
-                        .title("One-To-Many API")
-                        .description("API documentation with JWT authentication")
-                        .version("1.0"))
-                .servers(List.of(
-                        new Server().url("https://9116.32procr.amypo.ai/")
-                ))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", bearerAuth))
-                .addSecurityItem(new SecurityRequirement()
-                        .addList("bearerAuth"));
-    }
-}
